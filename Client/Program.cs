7a39006adc7302dab31c67715104c696fba19d93
filiptodorovic <todorovic.pr25.2019@uns.ac.rs>
2023-01-02 23:03:30 +0100

    Client has basic UI, reads actions from a file

--- Client/Program.cs
+++ Client/Program.cs
@@ -10,6 +10,73 @@ namespace Client
     {
         static void Main(string[] args)
         {
+            string key="";
+
+            while (key!="q") {
+                Console.WriteLine("\t====== MENU =======");
+                Console.WriteLine("[1] Choose an action to perform");
+                Console.WriteLine("[2] Manipulate the database");
+                Console.WriteLine("[q] Exit");
+                Console.WriteLine("Chose: ");
+
+                key = Console.ReadLine();
+
+                switch (key) {
+                    case "1":
+                        ChoseAction();
+                        break;
+                    case "2":
+                        ManupulateDatabase();
+                        break;
+                }
+            }
+
+        }
+
+        private static void ChoseAction() {
+            string input = "";
+            List<string> actions = XmlIO.DeSerializeObject<List<string>>("..\\..\\resourceFile.xml");
+            while (input != "q")
+            {
+                Console.WriteLine("\t++++++ACTIONS++++++");
+                for (int i = 0; i < actions.Count; i++)
+                {
+                    Console.WriteLine("({0}) {1}", i, actions[i]);
+                }
+                Console.WriteLine("(q) Exit actions menu");
+                Console.WriteLine("Chose: ");
+                input = Console.ReadLine();
+
+                //Log the action
+            }
+        }
+
+        private static void ManupulateDatabase()
+        {
+            string input = "";
+            while (input != "q")
+            {
+                Console.WriteLine("\t--DB MANIPULATION--");
+                Console.WriteLine("{1} Read events that I generated");
+                Console.WriteLine("{2} Read all events");
+                Console.WriteLine("{3} Update an event");
+                Console.WriteLine("{4} Supervise all events");
+                Console.WriteLine("{q} Exit DB manupulation");
+                input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "1":
+                        break;
+                    case "2":
+                        break;
+                    case "3":
+                        break;
+                    case "4":
+                        break;
+                }
+            }
+
         }
     }
 }
--- DataBase/Class1.cs
+++ DataBase/Class1.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace DataBase
 {
-    public class Class1
+    public class DataBase
     {
+
     }
 }
