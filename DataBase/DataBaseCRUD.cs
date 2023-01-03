4c81a2e2e85c8492a84fab4c547a4146802275c3
VidakGrujic <vidakspartanacgrujic@gmail.com>
2023-01-03 10:11:11 +0100

    Implemented DB CRUD operations.

---
+++ DataBase/DataBaseCRUD.cs
@@ -0,0 +1,161 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBase
+{
+    public class DataBaseCRUD : IDataBaseCRUD
+    {
+        public bool AddEntry(string filePath, DataBaseEntry entry)
+        {
+            try
+            {
+                string json = File.ReadAllText(filePath);
+                List<DataBaseEntry> entries = JsonConvert.DeserializeObject<List<DataBaseEntry>>(json);
+                if(!entries.Contains(entry))
+                {
+                    entries.Add(entry);
+                    json = JsonConvert.SerializeObject(entries, Formatting.Indented);
+                    File.WriteAllText(filePath, json);
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine("There is already entry with same uniqueId");
+                    return false;
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+                return false;
+            }
+        }
+
+        public bool DeleteEntryById(string filePath, int uniqueId)
+        {
+            bool returnValue = false;
+            try
+            {
+                string json = File.ReadAllText(filePath);
+                List<DataBaseEntry> entries = JsonConvert.DeserializeObject<List<DataBaseEntry>>(json);
+                DataBaseEntry entry = entries.Find(f => f.UniqueId == uniqueId);
+                if (entry != null)
+                {
+                    returnValue = entries.Remove(entry);
+                    json = JsonConvert.SerializeObject(entries, Formatting.Indented);
+                    File.WriteAllText(filePath, json);
+                    return returnValue;
+                }
+                return returnValue;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+                return false;
+            }
+        }
+
+        public bool DeleteEntryBySId(string filePath, string sId)
+        {
+            bool returnValue = false;
+            try
+            {
+                string json = File.ReadAllText(filePath);
+                List<DataBaseEntry> entries = JsonConvert.DeserializeObject<List<DataBaseEntry>>(json);
+                DataBaseEntry entry = entries.Find(f => f.SId == sId);
+                if (entry != null)
+                {
+                    returnValue = entries.Remove(entry);
+                    json = JsonConvert.SerializeObject(entries, Formatting.Indented);
+                    File.WriteAllText(filePath, json);
+                    return returnValue;
+                }
+                return returnValue;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+                return false;
+            }
+        }
+
+        public DataBaseEntry FindEntryById(string filePath, int uniqueId)
+        {
+            try
+            {
+                string json = File.ReadAllText(filePath);
+                List<DataBaseEntry> entries = JsonConvert.DeserializeObject<List<DataBaseEntry>>(json);
+                return entries.Find(f => f.UniqueId == uniqueId);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+                return null;
+            }
+        }
+
+        public DataBaseEntry FindEntryBySId(string filePath, string sId)
+        {
+            try
+            {
+                string json = File.ReadAllText(filePath);
+                List<DataBaseEntry> entries = JsonConvert.DeserializeObject<List<DataBaseEntry>>(json);
+                return entries.Find(f => f.SId == sId);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+                return null;
+            }
+        }
+
+        public bool ModifyEntry(string filePath, int uniqueId, DataBaseEntry entry)
+        {
+            bool returnValue = false;
+            try
+            {
+                string json = File.ReadAllText(filePath);
+                List<DataBaseEntry> entries = JsonConvert.DeserializeObject<List<DataBaseEntry>>(json);
+                foreach(DataBaseEntry e in entries)
+                {
+                    if(e.UniqueId == uniqueId)
+                    {
+                        returnValue = e.ModifyEntryObject(entry);
+                    }
+                }
+                json = JsonConvert.SerializeObject(entries, Formatting.Indented);
+                File.WriteAllText(filePath, json);
+                return returnValue;
+
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+                return false;
+            }
+        }
+
+        public List<DataBaseEntry> ReadAllEntries(string filePath)
+        {
+            List<DataBaseEntry> entries = new List<DataBaseEntry>();
+            try
+            {
+                string json = File.ReadAllText(filePath);
+                entries = JsonConvert.DeserializeObject<List<DataBaseEntry>>(json);
+                return entries;
+            } 
+            catch(Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+                return new List<DataBaseEntry>();
+            }
+        }
+    }
+}
--- Service/Program.cs
+++ Service/Program.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using DataBase;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Service
@@ -9,7 +12,7 @@ namespace Service
     class Program
     {
         static void Main(string[] args)
-        {
+        {      
         }
     }
 }
