157997f139396ba65ba6bf6160799642b8a9059e
VidakGrujic <vidakspartanacgrujic@gmail.com>
2023-01-05 12:43:43 +0100

    Added base code for RBAC model.

---
+++ SecurityManager/CustomAuthorizationManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Security.Principal; 
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecurityManager
+{
+    public class CustomAuthorizationManager : ServiceAuthorizationManager
+    {
+        protected override bool CheckAccessCore(OperationContext operationContext)
+        {
+            /*
+             * Maybe we will need this method
+            CustomPrincipal principal = operationContext.ServiceSecurityContext.
+                   AuthorizationContext.Properties["Principal"] as CustomPrincipal;
+            return principal.IsInRole("Read");
+            */
+            return true;
+
+        }
+    }
+}
--- SecurityManager/RolesConfigFile1.Designer.cs
+++ SecurityManager/RolesConfigFile1.Designer.cs
@@ -22,14 +22,14 @@ namespace SecurityManager {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class RolesConfigFIle {
+    internal class RolesConfigFile {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
         private static global::System.Globalization.CultureInfo resourceCulture;
         
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal RolesConfigFIle() {
+        internal RolesConfigFile() {
         }
         
         /// <summary>
@@ -39,7 +39,7 @@ namespace SecurityManager {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("SecurityManager.RolesConfigFIle", typeof(RolesConfigFIle).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("SecurityManager.RolesConfigFile", typeof(RolesConfigFile).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
